a06f785698bea41b528349656e1ed33ebff48210
najuml-bd <najmul0612@gmail.com>
2022-06-10 19:32:55 +0600

    API created to load all data

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
+
 
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woyff.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const projectsCollection = client.db("portfolio").collection("projects");
+
+        app.get('/projects', async (req, res) => {
+            const query = {};
+            const result = await projectsCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('portfolio server is running')
