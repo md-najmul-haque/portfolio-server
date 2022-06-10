b8351ab8c1f5845bf80cc910d34c93aef43e8200
najuml-bd <najmul0612@gmail.com>
2022-06-10 19:10:03 +0600

    server basic setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('portfolio server is running')
+})
+
+app.listen(port, console.dir('listening to port', port))
