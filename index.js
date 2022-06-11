2c1c4120213b58c26c05d21d9dd56e2b81312a99
najuml-bd <najmul0612@gmail.com>
2022-06-11 13:45:06 +0600

    cors issue solved

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+const corsConfig = {
+    origin: true,
+    credentials: true,
+}
+app.use(cors(corsConfig));
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woyff.mongodb.net/?retryWrites=true&w=majority`;
